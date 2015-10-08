0c6b8e9ba98550954439c7072723ef5829b22c46
Yuri Mikhaylov <me@yurijmi.ru>
2015-10-08 06:54:47 +0300

    Created Add Favorite View

---
+++ Favorite Places/AddFavoriteViewController.swift
@@ -0,0 +1,19 @@
+//
+//  AddFavoriteViewController.swift
+//  Favorite Places
+//
+//  Created by Юрий Михайлов on 08.10.15.
+//  Copyright © 2015 Юрий Михайлов. All rights reserved.
+//
+
+import UIKit
+
+class AddFavoriteViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+}
