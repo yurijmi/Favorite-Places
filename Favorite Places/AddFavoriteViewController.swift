8dc52a8b137e38adc3108b7bdd26c07997fee9cf
Yuri Mikhaylov <me@yurijmi.ru>
2015-10-08 07:08:32 +0300

    Saving in Add Favorite View

--- Favorite Places/AddFavoriteViewController.swift
+++ Favorite Places/AddFavoriteViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import MapKit
+import CoreData
 
 class AddFavoriteViewController: UIViewController {
     
@@ -25,6 +26,19 @@ class AddFavoriteViewController: UIViewController {
     }
     
     @IBAction func doneTapped(sender: AnyObject) {
+        let context = (UIApplication.sharedApplication().delegate as! AppDelegate).managedObjectContext
+        
+        let newFavorite = NSEntityDescription.insertNewObjectForEntityForName("Favorite", inManagedObjectContext: context) as! Favorite
+            newFavorite.name       = self.nameField.text
+            newFavorite.lat        = self.mapView.region.center.latitude
+            newFavorite.long       = self.mapView.region.center.longitude
+            newFavorite.latDelta   = self.mapView.region.span.latitudeDelta
+            newFavorite.longDelta  = self.mapView.region.span.longitudeDelta
+        
+        do {
+            try context.save()
+        } catch {}
+        
         self.dismissViewControllerAnimated(true, completion: nil)
     }
 
