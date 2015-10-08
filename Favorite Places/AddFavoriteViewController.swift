d7507d3c4d9a4f4f53d1a64b0ff6a446bf425c95
Yuri Mikhaylov <me@yurijmi.ru>
2015-10-08 07:00:29 +0300

    Updated Add Favorite

--- Favorite Places/AddFavoriteViewController.swift
+++ Favorite Places/AddFavoriteViewController.swift
@@ -7,13 +7,25 @@
 //
 
 import UIKit
+import MapKit
 
 class AddFavoriteViewController: UIViewController {
-
+    
+    @IBOutlet weak var nameField : UITextField!
+    @IBOutlet weak var mapView   : MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         
+    }
+    
+    @IBAction func cancelTapped(sender: AnyObject) {
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    @IBAction func doneTapped(sender: AnyObject) {
+        self.dismissViewControllerAnimated(true, completion: nil)
     }
 
 }
