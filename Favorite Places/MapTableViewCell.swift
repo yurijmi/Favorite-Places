4075ecd4419b76362cf8cc567e6b6e74ef4a1d2b
Yuri Mikhaylov <me@yurijmi.ru>
2015-10-08 06:18:51 +0300

    Added MapTableViewCell

---
+++ Favorite Places/MapTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  MapTableViewCell.swift
+//  Favorite Places
+//
+//  Created by Юрий Михайлов on 08.10.15.
+//  Copyright © 2015 Юрий Михайлов. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class MapTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var mapView: MKMapView!
+
+}
